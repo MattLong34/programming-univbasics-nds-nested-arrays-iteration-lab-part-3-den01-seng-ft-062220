e6de16e2a6f16130ed340eface64ad915697ddbd
Matt Long <mdlong34@gmail.com>
2020-06-02 16:29:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
       #if element is a string, shovel into new_string array
       if src[row_index][element_index].is_a? String
+#     if src[row_index][element_index].class == String
         new_string += src[row_index][element_index] += " "
       end
       element_index += 1 
@@ -14,22 +15,3 @@ def join_nested_strings(src)
   end
  new_string
 end
-
-
-
-
-# def join_nested_strings(src)
-#   final_string = ""
-#   row_index = 0
-#   while row_index < src.count do
-#     element_index = 0
-#     while element_index < src[row_index].count do      
-#       if src[row_index][element_index].class == String
-#         final_string += src[row_index][element_index] + ' '
-#       end
-#       element_index += 1
-#     end
-#     row_index += 1
-#   end
-#   final_string
-# end
